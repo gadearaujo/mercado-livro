1cda165d3d82523832e396f63aabdb5e7342ec00
gabriel <ga.dearaujo4@gmail.com>
2023-08-30 16:29:29 -0300

    Adicionado controller
    Metodos GET e POST
    
    -Adicionado Model
    
    -Adicionado PostCustomerRequest para retornar o que é necessario

---
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,24 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.model.CustomerModel
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("customer")
+class CustomerController {
+
+    @GetMapping
+    fun getCustomer(): CustomerModel {
+        return CustomerModel("1", "Gabriel", "ga.dearaujo4@gmail.com")
+    }
+
+    @PostMapping
+    fun create(@RequestBody customer : PostCustomerRequest) {
+        return println(customer)
+    }
+}
