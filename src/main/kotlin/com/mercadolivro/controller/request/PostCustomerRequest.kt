dd8baaf31f15159694450713c23554bab2aa8e17
gabriel <ga.dearaujo4@gmail.com>
2023-08-31 13:58:19 -0300

    Melhorias no codigo
    -Adicionado Extension

--- src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -1,5 +1,14 @@
 package com.mercadolivro.controller.request
+
+import com.mercadolivro.model.CustomerModel
+
 data class PostCustomerRequest (
     var name: String,
     var email: String
-)
+) {
+
+    fun toCustomerModel () : CustomerModel {
+        return CustomerModel(name = this.name, email = this.email)
+    }
+
+}
--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -1,11 +1,7 @@
 package com.mercadolivro.service
 
-import com.mercadolivro.controller.request.PostCustomerRequest
-import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.*
 
 @Service
 class CustomerService {
@@ -20,15 +16,17 @@ class CustomerService {
     }
 
 
-    fun create(customer : PostCustomerRequest) {
+    fun create(customer : CustomerModel) {
 
         val id = if (customers.isEmpty()) {
             "1"
         } else {
-            customers.last().id.toInt() + 1
+            customers.last().id!!.toInt() + 1
         }.toString()
 
-        customers.add(CustomerModel(id, customer.name, customer.email))
+        customer.id = id
+
+        customers.add(customer)
     }
 
     fun getCustomer(id: String): CustomerModel {
@@ -36,8 +34,8 @@ class CustomerService {
     }
 
 
-    fun update(id: String, customer : PutCustomerRequest) {
-        customers.filter { it.id == id }.first().let {
+    fun update(customer: CustomerModel) {
+        customers.filter { it.id == customer.id }.first().let {
             it.name = customer.name
             it.email = customer.email
         }
