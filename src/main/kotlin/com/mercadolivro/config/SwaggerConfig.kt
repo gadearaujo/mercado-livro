a58616bc81f9a1edae8e7d205924d0ae6feb5ba7
Gabriel <ga.dearaujo4@gmail.com>
2023-12-13 17:05:50 -0300

    Configurando Swagger

---
+++ src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
@@ -0,0 +1,21 @@
+package com.mercadolivro.config
+
+import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc
+
+
+@EnableSwagger2WebMvc
+class SwaggerConfig {
+
+    @Bean // utilizado o = para nao precisar abrir { da funcao
+    fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.mercadolivro.controller"))
+        .build()
+
+}
