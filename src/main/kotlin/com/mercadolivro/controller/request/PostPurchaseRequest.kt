324f12d4679bc7c293282841cae67bf4c125c69f
Gabriel <ga.dearaujo4@gmail.com>
2023-12-15 21:41:39 -0300

    Criando o event publisher
    -Criado Repository Purchase
    -Ajustes

--- src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.controller.request
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import jakarta.validation.constraints.Positive
 import org.jetbrains.annotations.NotNull
 
@@ -7,8 +8,10 @@ data class PostPurchaseRequest (
 
     @field:NotNull
     @field:Positive
+    @JsonAlias("customer_id") // utilizando snake_case
     val customerId: Int,
 
     @field:NotNull
+    @JsonAlias("book_ids") // utilizando snake_case
     val bookIds: Set<Int> // set se importa apenas com valores diferentes
 )
