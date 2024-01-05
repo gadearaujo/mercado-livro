d4ec859bc87529b4ffb7015368ae135607b302b1
Gabriel <ga.dearaujo4@gmail.com>
2024-01-05 01:51:43 -0300

    Criando tabela de Roles

---
+++ src/main/kotlin/com/mercadolivro/enums/Profile.kt
@@ -0,0 +1,8 @@
+package com.mercadolivro.enums
+
+enum class Profile (
+    val description: String
+) {
+    ADMIN("ADMIN_ROLE"),
+    CUSTOMER("CUSTOMER_ROLE")
+}
--- src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
+++ src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -1,6 +1,8 @@
 package com.mercadolivro.model
 
 import com.mercadolivro.enums.CustomerStatus
+import com.mercadolivro.enums.Profile
+
 import jakarta.persistence.*
 @Entity(name = "customer")
 
@@ -25,7 +27,13 @@ data class CustomerModel (
     var photoUrl: ByteArray,
 
     @Column
-    var password: String
+    var password: String,
+
+   @CollectionTable(name = "customer_roles", joinColumns = [JoinColumn(name="customer_id")]) // tabela que nao tem id
+   @ElementCollection(targetClass = Profile::class, fetch = FetchType.EAGER) // quando buscar na tabela customer, pegar as roles tb (com o FetchType.EAGER)
+   @Column(name = "role")
+   @Enumerated(EnumType.STRING)
+    var roles: Set<Profile> = setOf() // set vazio
 
 )
 
